0a5fb292c3c89cded599eb4ce0e04f3aaa911a54
amide-inc <aminuddin01697@gmail.com>
2021-11-27 23:29:05 +0530

    shallownet on amimals model completed, nedd to download datasets

--- plutoainetwork/shallownet_animals.py
+++ plutoainetwork/shallownet_animals.py
@@ -1,7 +1,9 @@
 from typing_extensions import Required
+from sklearn import metrics
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from tensorflow.keras import optimizers
 from plutoainetwork.preprocessing import ImageToArrayPreprocessor
 from plutoainetwork.preprocessing import SimplePreprocessor
 from plutoainetwork.datasets import SimpleDatasetsLoader
@@ -26,3 +28,16 @@ sdl = SimpleDatasetsLoader(preprocessors=[sp, iap])
 
 (data, labels) = sdl.load(imagePaths, verbose=500)
 data =  data.astype('float')/255
+
+
+trainX, testX, trainY, testY = train_test_split(data, labels, test_size=0.25, random_state=42)
+trainY = LabelBinarizer().fit_transform(trainY)
+testY = LabelBinarizer().fit_transform(testY)
+
+print("compiling model")
+opt = SGD(learning_rate=0.005)
+model = ShallowNet.build(width=32, height=32, depth=3, classes=3)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+print("training network")
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
